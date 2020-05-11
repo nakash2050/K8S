670fac91c58240855bc5a584b1f59d8ad7e9acc5
Nakash Sheikh <nakashsheikh@Nakashs-MBP.attlocal.net>
2020-05-11 16:55:10 -0500

    Added final CI/CD pipelines in Travis

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+docker build -t nakash2050/employee-spa:latest -t nakash2050/employee-spa:$SHA -f ./SPA/Dockerfile .
+docker build -t nakash2050/employee-microservice:latest -t nakash2050/employee-microservice:$SHA -f ./Registration/Dockerfile
+
+docker push nakash2050/employee-spa:latest
+docker push nakash2050/employee-spa:$SHA
+docker push nakash2050/employee-microservice
+docker push nakash2050/employee-microservice:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=nakash2050/employee-spa:$SHA
+kubectl set image deployments/server-deployment server=nakash2050/employee-microservice:$SHA
