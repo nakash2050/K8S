0041d68192a1d57d48a05138798d77fbddf3c13a
Nakash Sheikh <nakashsheikh@Nakashs-MBP.attlocal.net>
2020-05-11 17:39:07 -0500

    Missed adding build context for Registration Service

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t nakash2050/employee-spa:latest -t nakash2050/employee-spa:$SHA -f ./SPA/Dockerfile .
-docker build -t nakash2050/employee-microservice:latest -t nakash2050/employee-microservice:$SHA -f ./Registration/Dockerfile
+docker build -t nakash2050/employee-microservice:latest -t nakash2050/employee-microservice:$SHA -f ./Registration/Dockerfile .
 
 docker push nakash2050/employee-spa:latest
 docker push nakash2050/employee-spa:$SHA
